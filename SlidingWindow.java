de33abd8a8f704bec58820762366b5d3c71b946e
Vanieka Sharma <72801405+vanieka-sharma@users.noreply.github.com>
2022-04-23 22:12:41 +0530

    DSA_One

---
+++ SlidingWindow.java
@@ -0,0 +1,31 @@
+package dsa_one;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SlidingWindow {
+	static void countDistinctElements(int a[], int k)
+	{
+		Map<Integer,Integer> map = new HashMap<>();
+		for(int i=0;i<k;i++)
+		{
+			map.put(a[i], map.getOrDefault(a[i], 0)+1);
+		}
+		System.out.println(map.size());
+		for(int i=k;i<a.length;i++)
+		{
+			if(map.get(a[i-k])==1) map.remove(a[i-k]);
+			else map.put(a[i-k],map.get(a[i-k])-1);
+			
+			map.put(a[i], map.getOrDefault(a[i], 0)+1);
+			System.out.println(map.size());
+		}
+	}
+	public static void main(String[] args)
+	{
+		int a[] = {1,2,2,1,3,1,1,3};
+		int k = 4;
+		countDistinctElements(a,k);
+	}
+
+}
